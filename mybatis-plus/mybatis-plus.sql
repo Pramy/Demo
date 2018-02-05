43cf2b3c9e3312505fab5d03851d5e8fedf3c3fc
pramy <332538627@qq.com>
2018-02-05 19:40:15 +0800

    添加log4j2异步日志demo

---
+++ mybatis-plus/mybatis-plus.sql
@@ -0,0 +1,54 @@
+/*
+SQLyog Ultimate v11.33 (64 bit)
+MySQL - 5.7.19-log : Database - test
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`test` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `test`;
+
+/*Table structure for table `node` */
+
+DROP TABLE IF EXISTS `node`;
+
+CREATE TABLE `node` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) NOT NULL,
+  `parent_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `parent_id` (`parent_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COMMENT='节点表';
+
+/*Data for the table `node` */
+
+insert  into `node`(`id`,`name`,`parent_id`) values (1,'一级节点A',0),(2,'一级节点B',0),(3,'一级节点C',0),(4,'二级节点AA',1),(5,'二级节点aa',1),(6,'二级节点BB',2),(7,'三级级节点AAA',4),(8,'三级级节点aaa',4),(9,'三级级节点BBB',6);
+
+/*Table structure for table `person` */
+
+DROP TABLE IF EXISTS `person`;
+
+CREATE TABLE `person` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `address` varchar(255) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+/*Data for the table `person` */
+
+insert  into `person`(`id`,`address`,`age`,`name`) values (1,'home',20,'pramy'),(3,'school',20,'汪云飞'),(4,'school',21,'pramy'),(5,'school',22,'pramy'),(6,'school',20,'pramy');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
